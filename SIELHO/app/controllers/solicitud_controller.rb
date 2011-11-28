00deae8d928dbde8d2982d2fb7d8eac3d55a7d07
Jose Matute <jmatuteflores@gmail.com>
2011-11-28 09:56:30 -0800

    solicitudes para personas, falta tipo de documentos

--- SIELHO/app/controllers/solicitud_controller.rb
+++ SIELHO/app/controllers/solicitud_controller.rb
@@ -5,17 +5,18 @@ class SolicitudController < ApplicationController
 		@instituciones = Institucion.instituciones()
 		@ocupaciones = Ocupacion.ocupaciones()
 		@solicitud = Solicitud.new
+		@documento = Documento.new
 	end
 
 	def solicitud_juridica
 		@solicitante = Solicitante.new
 		@solicitud = Solicitud.new
+		@ocupaciones = Ocupacion.ocupaciones()
 		@instituciones = Institucion.instituciones()
-
+		@documento = Documento.new
 	end
 
 	def create
-
 		redirect_to root_path
 	end
 end
