62e263d80b35b2a558192fa029208f82c4702944
javier <javier@javier-HP-Compaq-6715b-KL428LA-ABM.(none)>
2011-11-30 09:40:41 -0600

    archivos hecho

--- SIELHO/app/models/mensaje.rb
+++ SIELHO/app/models/mensaje.rb
@@ -1,7 +1,9 @@
 class Mensaje < ActiveRecord::Base
 
-has_attached_file :document
+has_attached_file :document ,:path => ":rails_root/public/:class/:attachment/:id/:style_:basename.:extension"
+
 attr_accessor :avatar_file_name
+
 
 	validates_presence_of :destinatario_id,:emisor_id,:titulo,:texto,:fecha
 	def self.HasMensajeNoLeido(usuario)
