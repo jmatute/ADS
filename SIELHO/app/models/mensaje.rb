d99b8311bb08612893a2cdfe93e4f86a42a6a8ed
javier <javier@javier-HP-Compaq-6715b-KL428LA-ABM.(none)>
2011-11-30 08:28:33 -0600

    Subida de archivos :D

--- SIELHO/app/models/mensaje.rb
+++ SIELHO/app/models/mensaje.rb
@@ -1,4 +1,8 @@
 class Mensaje < ActiveRecord::Base
+
+has_attached_file :document
+attr_accessor :avatar_file_name
+
 	validates_presence_of :destinatario_id,:emisor_id,:titulo,:texto,:fecha
 	def self.HasMensajeNoLeido(usuario)
 		return Mensaje.where(:leido=>false,:borrado=>false,:destinatario_id=>usuario) != []
