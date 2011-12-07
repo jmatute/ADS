b0efd1f8c282f6afe17a897e7976081e95a30dc2
javier <javier@javier-HP-Compaq-6715b-KL428LA-ABM.(none)>
2011-12-07 13:12:24 -0600

    asd

--- SIELHO/app/controllers/buscar_controller.rb
+++ SIELHO/app/controllers/buscar_controller.rb
@@ -1,14 +1,8 @@
 class BuscarController < ApplicationController
   def index
-#	p params[:inicio]["(1i)"]
- #       p params[:inicio]["(2i)"]
-#	p params[:inicio]["(3i)"]
-
-#	p "asdddddddddddddddddddddddddddddddddddd"
 
 	unless params[:institucion].nil?
-	@encontrados = Solicitud.buscar(params[:search],params[:institucion]["institucion"],params[:inicio],params[:final])
-  
+	@encontrados = Solicitud.buscar(params[:search],params[:institucion]["institucion"],params[:inicio],params[:final]) 
 	end
   end
   def create
